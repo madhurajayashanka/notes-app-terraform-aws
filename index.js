47e0e53a4bc0041154d3870f656fe55c1d974d11
Madhura Jayashanka <madhuraweerasooriye@gmail.com>
2024-05-30 12:33:02 +0530

    builder added

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
